359b3b214981898d0ecf24f6ae7fd2e4253f61cf
Victor Shevchenko <13shev.js@gmail.com>
2022-02-25 00:43:02 +0300

    feat: task solution

--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
+module.exports = function towelSort(matrix) {
+  if (matrix === undefined) return [];
 
-// You should implement your task here.
-
-module.exports = function towelSort (matrix) {
-  return [];
-}
+  let result = [];
+  for (let i = 0; i < matrix.length; i++) {
+    if (i % 2) matrix[i].sort((a, b) => b - a);
+    else matrix[i].sort((a, b) => a - b);
+    result = result.concat(matrix[i]);
+  }
+  return result;
+};
